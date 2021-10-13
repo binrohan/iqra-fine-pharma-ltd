2191b7a1216c7006e92ffa373305a0c07c332420
binrohan <binrohan.cs@gmail.com>
2021-10-13 17:04:50 +0600

    problem with overflow

--- dist/js/style.js
+++ dist/js/style.js
@@ -1,6 +1,15 @@
 const cartOpenBtn = document.querySelector('#cart-open-btn');
 const cartCloseBtn = document.querySelector('#cart-close-btn');
 const cartExpan = document.querySelector('#cart-expan');
+const drawer = document.querySelector('#js--drawer');
+
+document.querySelector('#drawer-toggle-btn').addEventListener('click', () => {
+    openSideDrawer();
+});
+
+document.querySelector('.drawer__categories__category--header__close').addEventListener('click', () => {
+    openSideDrawer();
+});
 
 cartOpenBtn.addEventListener('click', () => {
     openCart();
@@ -20,4 +29,13 @@ const openCart = () => {
 const closeCart = () => {
     cartOpenBtn.style.display = 'block';
     cartExpan.style.display = 'none';
+}
+
+const openSideDrawer = () => {
+    const bodyEl = document.querySelector('body');
+
+    drawer.classList.toggle('drawer-open');
+    bodyEl.style.position = bodyEl.style.position === 'sticky' ? 'relative' : 'sticky';
+    bodyEl.style.overflow = bodyEl.style.overflow === 'hidden' ? 'visible' : 'hidden';
+    bodyEl.style.maxHeight = bodyEl.style.maxHeight === '' ? '100vh' : '';
 }
